3f42db301931a36fe918ed82c414506c786abf85
PHarr_mx <pharr.mx@outlook.com>
2023-08-24 16:20:34 +0800

    取整

---
+++ src/编程技巧和基础算法/语法杂项/取整.cpp
@@ -0,0 +1,8 @@
+template <typename T, typename U>
+T ceil(T x, U y) {
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+template <typename T, typename U>
+T floor(T x, U y) {
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
